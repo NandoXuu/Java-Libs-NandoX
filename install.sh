35a06b3e02615fef255afbd437a8ae290caafffb
NandoX LUA 🌙 <nand00xustar1@gmail.com>
2025-07-03 23:22:19 -0300

    Update and rename Lib_script.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 clear
-echo "🛠️ Installing Java 17 (JDK) in Termux..."
+echo "⚙ Installing Java 17..."
 mkdir -p $HOME/jre && cd $HOME/jre
-echo "↓ Downloading Java JDK 17..."
 wget https://github.com/Hax4us/java/releases/download/v8/jdk17.tar.gz -O jdk17.tar.gz
-echo " Extracting JDK..."
 tar -xvzf jdk17.tar.gz
 rm jdk17.tar.gz
-echo "⚙ Adding Java to PATH..."
 PROFILE_FILE="$HOME/.bashrc"
-if [ ! -f "$PROFILE_FILE" ]; then
-  PROFILE_FILE="$HOME/.zshrc"
-fi
+if [ ! -f "$PROFILE_FILE" ]; then PROFILE_FILE="$HOME/.zshrc"; fi
 if ! grep -q 'jdk17/bin' "$PROFILE_FILE"; then
   echo 'export PATH=$HOME/jre/jdk17/bin:$PATH' >> "$PROFILE_FILE"
 fi
 export PATH=$HOME/jre/jdk17/bin:$PATH
-echo "✓ Verifying Java installation..."
 java -version
-echo "Java 17 is now installed and ready to use in Termux!"
+echo "Java installed!"
+
+echo "↓ Cloning repository..."
+git clone https://github.com/NandoXuu/Java-Libs-NandoX.git ~/JavaLibsNX
+cd ~/JavaLibsNX
+
+echo "! Running main.py..."
+if [ -f "main.py" ]; then
+  python main.py
+else
+  echo "main.py not found!"
+fi
