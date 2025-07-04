98fa2671e2c24c0b6d4ef584815b30c68c6b4508
NandoX LUA 🌙 <nand00xustar1@gmail.com>
2025-07-03 23:43:32 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,2 @@
-import os
-
-print("=== Welcome to Java Libs NandoX ===")
-print("⚙ Listing repository contents:\n")
-
-files = os.listdir()
-for file in files:
-    print("> " + file)
-
-print("\n✓ Script executed successfully.")
+print("Java JDK was installed successfully! ✓")
+print("Ready to code!")
